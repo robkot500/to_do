e8075ba2a4379a0260f2ffe20fd2c0a9fa8671d8
robkot500 <robert.kotwicki5@gmail.com>
2020-07-19 19:58:43 +0200

    Updates

---
+++ precache-manifest.948a9b06df169f78751c33d4b379d0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c55881e5e06ced77151d75f47320993",
+    "url": "/to_do/index.html"
+  },
+  {
+    "revision": "9c6cfdc5d2c08c298825",
+    "url": "/to_do/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3923b63aa8baeafe99f1",
+    "url": "/to_do/static/js/2.d00c121a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do/static/js/2.d00c121a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6cfdc5d2c08c298825",
+    "url": "/to_do/static/js/main.b3683315.chunk.js"
+  },
+  {
+    "revision": "5b6fe0a39d95288cbfe4",
+    "url": "/to_do/static/js/runtime-main.8663b774.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to_do/static/media/logo.5d5d9eef.svg"
+  }
+]);
