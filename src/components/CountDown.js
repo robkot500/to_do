0de7d098357e1fefa5016ff3acc82fcc45b4ca55
robkot500 <robert.kotwicki5@gmail.com>
2020-08-19 06:43:35 +0200

    working countdown

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -10,13 +10,18 @@ function CountDown(props) {
         return props.items.selectedItem.id === each.id
     })
     console.log(newCountDown[0].alarm.iconOn);
-    const date = newCountDown[0].date;
+    // const date = newCountDown[0].date;
 
-    const [countDownDate, setCountDownDate] = useState(date)
+    // if (newCountDown[0].alarm.iconOn === false) { setCountDownDate(null) }
+
+    if (newCountDown[0].date >= new Date()) { console.log('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu'); }
+
+    const initDate = newCountDown[0] && newCountDown[0].date >= new Date() ? (newCountDown[0].date) : (null)
+
+    const [countDownDate, setCountDownDate] = useState(initDate)
     // var countDownDate = new Date("Aug 17, 2020 16:40:25").getTime();
 
     // const countDownDate = newCountDown[0].date;
-
     console.log(countDownDate);
     // Update the count down every 1 second
     useEffect(() => {
@@ -37,8 +42,8 @@ function CountDown(props) {
             console.log(days + "d " + hours + "h "
                 + minutes + "m " + seconds + "s ")
 
-            if (distance < 0) {
-                console.log(newCountDown[0].id);
+            if (distance < 0 && newCountDown[0].date - new Date() <= 3000 && newCountDown[0].date - new Date() >= -3000) {
+                console.log('xxxxxxxxxxxxxxxxxxxxxxxx', newCountDown[0].date - new Date());
                 props.alarmOn(newCountDown[0].id)
                 setTimeout(function () { props.alarmOff(newCountDown[0].id) }, 5000);
                 clearInterval(x);
@@ -47,8 +52,15 @@ function CountDown(props) {
             console.log(newCountDown[0].alarm.iconOn);
 
             if (newCountDown[0].alarm.iconOn === false) { console.log('ccccccccccccccccccccccccc'); clearInterval(x); }
+            const clearInt = () => { console.log('ccccccccccccccccccccccccc'); clearInterval(x); }
+            // if (newCountDown[0].date - new Date() >= 3000) { console.log('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'); }
+
         }, 1000);
-    }, []);
+
+        return () => {
+            clearInterval(x)
+        }
+    }, [newCountDown[0].alarm.iconOn]);
 
 
 
